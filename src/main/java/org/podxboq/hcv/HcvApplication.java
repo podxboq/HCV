1b01b5c732af2183feb958a8c95568f5e12d9373
podxboq <podxboq@gmail.com>
2024-07-09 22:28:09 +0200

    Alta del proyecto

---
+++ src/main/java/org/podxboq/hcv/HcvApplication.java
@@ -0,0 +1,13 @@
+package org.podxboq.hcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/podxboq/hcv/HcvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.podxboq.hcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
