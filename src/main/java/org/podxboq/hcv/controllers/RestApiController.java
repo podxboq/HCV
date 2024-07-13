3c0f3ed4bfc0ff7bee4372449f7426bc94e2f94c
podxboq <podxboq@gmail.com>
2024-07-13 13:37:18 +0200

    Llamada post a la api

--- src/main/java/org/podxboq/hcv/controllers/RestApiController.java
+++ src/main/java/org/podxboq/hcv/controllers/RestApiController.java
@@ -6,9 +6,7 @@ import org.podxboq.hcv.models.Mascota;
 import org.podxboq.hcv.repositories.IngresosRepository;
 import org.podxboq.hcv.repositories.MascotasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -22,6 +20,12 @@ public class RestApiController {
 	@GetMapping("/ingreso")
 	public Iterable<Ingreso> ingreso() {
 		return ingresosRepository.findAll();
+	}
+
+	@PostMapping(value = "/ingreso", consumes = "application/json")
+	public Ingreso ingresoAlta(@RequestBody Ingreso ingreso) {
+
+		return ingresosRepository.save(ingreso);
 	}
 
 	@GetMapping("/mascota/{idMascota}")
